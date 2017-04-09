48d39a0f9a32c00f618cc1c8182c496082bafd12
ejprice54321 <emma.price@students.olin.edu>
2017-04-09 16:26:53 -0400

    removed executables

--- 3D/PipeGrowth/animation3d.cpp
+++ 3D/PipeGrowth/animation3d.cpp
@@ -1,14 +1,15 @@
 /*
+ * The general set-up of this file originated from:
  * OGL02Animation.cpp: 3D Shapes with animation
+ * significant changes have been made within openGL functions
+  * to alter the functionality of the code
  */
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
 
 /* Global variables */
-char title[] = "3D Shapes with animation";
-GLfloat anglePyramid = 0.0f;  // Rotational angle for pyramid [NEW]
-GLfloat cubeLocale = 1.0f;     // Rotational angle for cube [NEW]
-GLfloat cubeLocale2 = -3.0f;     // Rotational angle for cube [NEW]
-int refreshMills = 15;        // refresh interval in milliseconds [NEW]
+char title[] = "3D Cube with animation";
+GLfloat cubeLocale = 1.0f;     // Length of the cude
+int refreshMills = 15;        // refresh interval in milliseconds
 
 /* Initialize OpenGL Graphics */
 void initGL() {
@@ -28,18 +29,18 @@ void display() {
 
    // Render a color-cube consisting of 6 quads with different colors
    glLoadIdentity();                 // Reset the model-view matrix
-   glTranslatef(0.0f, cubeLocale2, -7.0f);
-     // Move right and into the screen
+   glTranslatef(-3.0f, 0.0f, -7.0f);
+  // Move right and into the screen
   //  glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  // Rotate about (1,1,1)-axis [NEW]
 
-   glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+   glBegin(GL_QUADS); // Begin drawing the square
 
       // Front face  (z = 1.0f)
-      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-      glVertex3f( cubeLocale,  1.0f, 1.0f);
-      glVertex3f(-1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f, -1.0f, 1.0f);
-      glVertex3f( cubeLocale, -1.0f, 1.0f);
+      glColor3f(1.0f, 0.0f, 0.0f);// Red
+      glVertex3f( cubeLocale,  0.25f, 0.25f);
+      glVertex3f(-0.25f,  0.25f, 0.25f);
+      glVertex3f(-0.25f, -0.25f, 0.25f);
+      glVertex3f( cubeLocale, -0.25f, 0.25f);
 
    glEnd();  // End of drawing color-cube
 
@@ -48,7 +49,6 @@ void display() {
    // Update the rotational angle after each refresh [NEW]
 
     cubeLocale += 0.0125f;
-    // cubeLocale2 += 0.025f;
 }
 
 /* Called back when timer expired [NEW] */
