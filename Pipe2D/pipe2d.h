a9afaf888fc79b0acc6da956fc729d3d609f6e68
ejprice54321 <emma.price@students.olin.edu>
2017-04-24 11:59:08 -0400

    adding pipe

---
+++ Pipe2D/pipe2d.h
@@ -0,0 +1,42 @@
+#ifndef PIPE_H
+#define PIPE_H
+
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// A macro for unused variables (to bypass those pesky G++ warnings)
+#define UNUSED(param) (void)(param)
+#define HEIGHT 59
+#define WIDTH 59
+
+deque<deque<float> > part_coords;
+vector<int> directions;
+int map[WIDTH+2][HEIGHT+2];
+
+GLubyte red;
+GLubyte green;
+GLubyte blue;
+
+const char *title;
+float map_half_length ;
+int direction;
+int move_speed;
+bool moved;
+int growth_stage;
+int growth;
+int last_direction;
+
+void grow(int value);
+void reset();
+void constructMap();
+void printMap();
+
+//Opengl functions
+void initGL();
+void reshape(GLsizei width, GLsizei height);
+void display();
+
+#endif //PIPE_H
